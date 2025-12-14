625591377f24886bb4cc5e470b11371fbcd58342
Akshit <91170781+HYakshit@users.noreply.github.com>
2025-12-14 21:19:12 +0530

    fix auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./hooks/AuthContext";
+import { NotesProvider } from "./hooks/NotesContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <NotesProvider>
+          <App />
+        </NotesProvider>
+      </AuthProvider>
     </Router>
   </StrictMode>
 );
