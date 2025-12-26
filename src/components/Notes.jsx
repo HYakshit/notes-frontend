1c172a23e4e8f81913e5b57a06162e654a56dc3f
Akshit <91170781+HYakshit@users.noreply.github.com>
2025-12-26 21:20:24 +0530

    feat:f

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -7,7 +7,7 @@ const Notes = () => {
 
   if (loading) {
     return (
-      <div className="flex dark:bg-gray-900 dark:text-white  justify-center items-center h-screen">
+      <div className="flex text-black dark:bg-gray-900 dark:text-white  justify-center items-center h-screen">
         Loading...
       </div>
     );
@@ -18,7 +18,7 @@ const Notes = () => {
       {!notes || notes.length === 0 ? (
         <>
 
-          <div className="flex justify-center dark:bg-gray-600 dark:text-white items-center h-screen">
+          <div className="flex text-black justify-center dark:bg-gray-600 dark:text-white items-center h-screen">
             No notes available. {<AddNote justifyDirection="center" />}
           </div>
         </>
--- src/components/PinedNotes.jsx
+++ src/components/PinedNotes.jsx
@@ -7,7 +7,7 @@ export const PinedNotes = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center  dark:bg-gray-600 dark:text-white items-center h-screen">
+      <div className="flex text-black justify-center  dark:bg-gray-600 dark:text-white items-center h-screen">
         Loading...
       </div>
     );
@@ -16,7 +16,7 @@ export const PinedNotes = () => {
   const pinnedNotes = notes?.filter((note) => note.pinned);
   if (!pinnedNotes || pinnedNotes.length === 0) {
     return (
-      <div className="flex justify-center  dark:bg-gray-600 dark:text-white items-center h-screen">
+      <div className="flex text-black justify-center  dark:bg-gray-600 dark:text-white items-center h-screen">
         No pinned notes available. {<AddNote justifyDirection="center" setNotes={setNotes} />}
       </div>
     );
